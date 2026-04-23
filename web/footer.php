d7e726874123e7032b5f9d94edaf10fc75b868c9
a25adrtomdie <a25adrtomdie@inspedralbes.cat>
2026-04-23 14:18:05 +0200

    Creacion de docker y base de datos. Añado index header y footer de practica de DIGISOS para probar.

---
+++ web/footer.php
@@ -0,0 +1,12 @@
+</main>
+<footer class="px-2 py-2 fixed-bottom bg-dark">
+        <span class="text-muted">CRUD de MySQLi con PHP creado por
+            <a class="text-white" href="https://parzibyte.me/blog/">Parzibyte</a>
+            &nbsp;|&nbsp;
+            <a target="_blank" class="text-white" href="https://github.com/parzibyte/crud-php-mysqli">
+                Ver código fuente
+            </a>
+        </span>
+</footer>
+</body>
+</html>
---
+++ web/index.php
@@ -0,0 +1,8 @@
+<?php include_once "header.php";?>
+<h1>Benvigut a l'aplicació de gestió d'incidències</h1>
+<p>Tria una de les dos opcions:</p>
+
+<button class="btn btn-primary"><a href="mostrar.php" class="text-light">Mostrar videojocs</a></button>
+<button class="btn btn-primary"><a href="insertar.php" class="text-light">Afegir videojocs</a></button>
+
+<?php include_once "footer.php";?>
