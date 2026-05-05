110cbf3bc1628b89afc5d2f0fd37c4e64d8d2bca
Paubarlim <a25paubarlim@inspedralbes.cat>
2026-05-05 10:23:01 +0200

    Cambios manuales

--- web/index.php
+++ web/index.php
@@ -1,16 +1,14 @@
 <?php include_once "header.php";?>
 
-
-
-<div class="mx-auto text-center mt-5" style="width: 200px;">
-<img src="../img/logoGI3P.png" alt="logo proyecte">    
-<h1>Benvigunt!</h1>
+<div class="mx-auto mt-5 text-center" style="width: 200px;">
+    <img src="../img/logoGI3P.png" alt="logo projecte">    
+    <h1>Benvingut!</h1>
     <p>Tria una de les opcions:</p>
     
-    <div class="row gap-2">
-        <button class="btn btn-primary"><a href="CrearIncidUser.php" class="text-light">Usuari (professor)</a></button>
-        <button class="btn btn-primary"><a href="tecnic.php" class="text-light">Tècnic</a></button>
-        <button class="btn btn-primary"><a href="admin.php" class="text-light">Administrador (root)</a></button>
+    <div class="d-grid gap-2">
+        <button class="btn btn-primary btn-index"><a href="CrearIncidUser.php" class="text-light">Usuari (professor)</a></button>
+        <button class="btn btn-primary btn-index"><a href="tecnic.php" class="text-light">Tècnic</a></button>
+        <button class="btn btn-primary btn-index"><a href="admin.php" class="text-light">Administrador (root)</a></button>
     </div>
 </div>
 
