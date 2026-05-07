4c7b2b069bdc23329e5012d7372c3e68f1f57644
Paubarlim <a25paubarlim@inspedralbes.cat>
2026-05-07 22:15:44 +0200

    canvis a má del fitxers

--- web/admin/admin.php
+++ web/admin/admin.php
@@ -1,14 +1,14 @@
 <?php require_once "../header.php"; ?>
 
-<link rel="stylesheet" href="../css/responsive.css">
-
-<div class="w-25 p-3"> <!--widht + padding-->
-    <div class="d-grid gap-4">
-        <a href="../index.php" class="btn rounded text-white btn-index" style="background-color:#278DE6">INICI</a>
-        <a href="listTecnics.php" class="btn rounded text-white btn-index" style="background-color:#278DE6">TROBA UN TÈCNIC</a>
-        <a href="listIncidAdmin.php" class="btn rounded text-white btn-index" style="background-color:#278DE6">LLISTAR INCIDÈNCIES</a>
-        <a href="" class="btn rounded text-white btn-index" style="background-color:#278DE6">ASSIGNAR INCIDÈNCIES</a>
+<div class="tabla-responsive">
+    <div class="w-25 p-3"> <!--widht + padding-->
+        <div class="d-grid gap-4">
+            <a href="listTecnics.php" class="btn rounded text-white btn-index" style="background-color:#278DE6">TROBA UN TÈCNIC</a>
+            <a href="listIncidAdmin.php" class="btn rounded text-white btn-index" style="background-color:#278DE6">LLISTAR INCIDÈNCIES</a>
+            <a href="" class="btn rounded text-white btn-index" style="background-color:#278DE6">ASSIGNAR INCIDÈNCIES</a>
+        </div>
     </div>
+    <a href="../index.php" class="btn rounded text-white btn-index" style="background-color:#278DE6">INICI</a>
 </div>
 
- <?php require_once "../footer.php"; ?>
+<?php require_once "../footer.php"; ?>
